65519d85ba6f6b36b7bc503e49fdc719064c98f8
tejudevops <saitejaswini8121@gmail.com>
2025-05-30 21:57:59 +0530

    creating prometheus server through terraform

---
+++ prometheus/main.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "centos8" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = [973714476881]
+}
+
+terraform {
+  backend "s3" {
+    bucket = "teju-devops"
+    key    = "prometheus/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "prometheus" {
+  ami           = data.aws_ami.centos8.image_id
+  instance_type = "small"
+  vpc_security_group_ids = []
+}
+
+resource "aws_route53_record" "prometheus" {
+  zone_id = "Z07191123NJU9NTTKKZJ1"
+  name    = "prometheus"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.prometheus.private_ip]
+}
+
+resource "aws_route53_record" "prometheus-public" {
+  zone_id = "Z07191123NJU9NTTKKZJ1"
+  name    = "prometheus-public"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.prometheus.public_ip]
+}
