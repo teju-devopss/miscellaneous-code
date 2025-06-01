584136ab1342b07fe0ffe2196cdff977853732f6
tejudevops <saitejaswini8121@gmail.com>
2025-06-01 12:01:33 +0530

    Fetching credentials from aws parameter store

--- ansible.sh
+++ ansible.sh
@@ -2,3 +2,7 @@ ssh_username=$(aws ssm get-parameter --name "ssh_username" --with-decryption --q
 ssh_password=$(aws ssm get-parameter --name "ssh.ssh_password" --with-decryption --query "Parameter.Value" --output text)
 
 ansible-playbook -i 172.31.35.165, prometheus.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
+
+
+
+# this we are using for fetching passwords for aws ssm parameters
