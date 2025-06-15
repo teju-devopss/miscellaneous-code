24904c21e8c6caf1989804bf88b35f782a90227d
tejudevops <saitejaswini8121@gmail.com>
2025-06-15 17:47:32 +0530

    elastic search

--- elasticsearch/ansible.sh
+++ elasticsearch/ansible.sh
@@ -1,7 +1,7 @@
 ssh_username=$(aws ssm get-parameter --name "ssh.username" --with-decryption --query 'Parameter.Value' --output text)
 ssh_password=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
 
-ansible-playbook -i 172.31.20.63, elasticsearch.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
+ansible-playbook -i 172.31.19.66, elasticsearch.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
 
 
 
