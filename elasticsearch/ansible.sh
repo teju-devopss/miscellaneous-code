72b8a3d7adc3774cc43e67022c27e267685e2aed
tejudevops <saitejaswini8121@gmail.com>
2025-06-15 17:26:01 +0530

    elastic search

---
+++ elasticsearch/ansible.sh
@@ -0,0 +1,8 @@
+ssh_username=$(aws ssm get-parameter --name "ssh.username" --with-decryption --query 'Parameter.Value' --output text)
+ssh_password=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
+
+ansible-playbook -i 172.31.35.165, prometheus.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
+
+
+
+# this we are using for fetching passwords for aws ssm parameters
