9d86174f295572554fbc66d878e8494bd00abf95
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-04 15:42:28 +0530

    creating tools lb

--- variables.tf
+++ variables.tf
@@ -5,16 +5,16 @@ variable "tools" {
       port          = 9000
       priority      = 100
     }
-    elasticsearch = {
-      instance_type = "m6in.large"
-      port          = 80
-      priority      = 101
-    }
-    prometheus = {
-      instance_type = "t3.small"
-      port          = 9090
-      priority      = 102
-    }
+    # elasticsearch = {
+    #   instance_type = "m6in.large"
+    #   port          = 80
+    #   priority      = 101
+    # }
+    # prometheus = {
+    #   instance_type = "t3.small"
+    #   port          = 9090
+    #   priority      = 102
+    # }
   }
 }
 
