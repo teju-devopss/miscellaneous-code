f04129644432dd95d550dc538c950952ea2cd0c3
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-04 12:49:12 +0530

    sonarqube ansible code

--- ansible.sh
+++ ansible.sh
@@ -2,5 +2,5 @@ ssh_username=$(aws ssm get-parameter --name "ssh.username" --with-decryption --q
 ssh_password=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
 
 tool_name=$1
-IP=$(aws ec2 describe-instances --filters Name=tag.Name,Values=$tool_name --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-ansible-playbook -i ${IP}, elasticsearch.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password -e tool_name=$tool_name
+IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$tool_name --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+ansible-playbook -i ${IP}, playbook.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password -e tool_name=$tool_name
