b1eb9fb7a651dd30a2aad21388dc198c8f50d361
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-04 15:27:05 +0530

    creating tools lb

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2" {
-  ami           = data.aws_ami.centos8.image_id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.centos8.image_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = ["sg-05c761dbd36f70805"]
-  subnet_id = "subnet-03b0995466039e839"
+  subnet_id              = "subnet-03b0995466039e839"
 
   instance_market_options {
     market_type = "spot"
@@ -10,8 +10,9 @@ resource "aws_instance" "ec2" {
       instance_interruption_behavior = "stop"
       spot_instance_type             = "persistent"
     }
-
   }
+
+
   tags = {
     Name = var.tool
   }
@@ -40,14 +41,18 @@ resource "aws_lb_listener_rule" "rule" {
     }
   }
 }
+
 resource "aws_lb_target_group" "tg" {
   name     = "${var.tool}-tg"
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
-resource "aws_lb_target_group_attachment" "test" {
+
+resource "aws_lb_target_group_attachment" "attach" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.ec2.id
   port             = var.port
 }
+
+
--- variables.tf
+++ variables.tf
@@ -2,8 +2,18 @@ variable "tools" {
   default = {
     sonarqube = {
       instance_type = "t3.large"
-      port =9000
-      priority = 100
+      port          = 9000
+      priority      = 100
+    }
+    elasticsearch = {
+      instance_type = "m6in.large"
+      port          = 80
+      priority      = 101
+    }
+    prometheus = {
+      instance_type = "t3.small"
+      port          = 9090
+      priority      = 102
     }
   }
 }
