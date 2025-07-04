220a156efb346af1714f010f2e21b350219e064b
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-04 12:18:26 +0530

    making code dry

--- ansible.sh
+++ ansible.sh
@@ -3,4 +3,4 @@ ssh_password=$(aws ssm get-parameter --name "ssh.password" --with-decryption --q
 
 tool_name=$1
 IP=$(aws ec2 describe-instances --filters Name=tag.Name,Values=$tool_name --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-ansible-playbook -i ${IP}, elasticsearch.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
+ansible-playbook -i ${IP}, elasticsearch.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password -e tool_name=$tool_name
