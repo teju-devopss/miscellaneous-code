d76384d4a8ee7f5580931518b8c6ef0f61ef8c43
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-04 15:40:06 +0530

    creating tools lb

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "ec2" {
-  source = "./modules/ec2"
-  for_each = var.tools
-  tool = each.key
+  source        = "./modules/ec2"
+  for_each      = var.tools
+  tool          = each.key
   instance_type = each.value["instance_type"]
-  dns_name = module.alb.dns_name
-  listener_arn = module.alb.listener
-
-  port     = each.value["port"]
-  priority = each.value["priority"]
+  dns_name      = module.alb.dns_name
+  listener_arn  = module.alb.listener
+  port          = each.value["port"]
+  priority      = each.value["priority"]
 }
- module "alb" {
-   source = "./modules/alb"
- }
+
+module "alb" {
+  source = "./modules/alb"
+}
