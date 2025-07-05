100b594864e58f0ff68a3ff341318d8f0fd42439
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-05 15:00:57 +0530

    adding instances

--- variables.tf
+++ variables.tf
@@ -15,6 +15,21 @@ variable "tools" {
       port          = 9090
       priority      = 102
     }
+    grafana = {
+      instance_type = "t3.small"
+      port          = 3000
+      priority      = 102
+    }
+    alertmanager = {
+      instance_type = "t3.small"
+      port          = 9093
+      priority      = 102
+    }
+    artifactory = {
+      instance_type = "t3.medium"
+      port          = 8081
+      priority      = 102
+    }
   }
 }
 
