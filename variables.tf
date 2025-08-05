8fe26be37b04e87284265ba246d8e0dac884d408
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-05 11:24:56 +0530

    roboshop payment

--- variables.tf
+++ variables.tf
@@ -1,41 +1,41 @@
 variable "tools" {
   default = {
-    # sonarqube = {
-    #   instance_type = "t3.large"
-    #   port          = 9000
-    #   priority      = 100
-    #   policy_list   =[]
-    # }
-    # elasticsearch = {
-    #   instance_type = "m6in.large"
-    #   port          = 80
-    #   priority      = 101
-    #   policy_list   =[]
-    # }
-    # prometheus = {
-    #   instance_type = "t3.small"
-    #   port          = 9090
-    #   priority      = 102
-    #   policy_list   =[]
-    # }
-    # grafana = {
-    #   instance_type = "t3.small"
-    #   port          = 3000
-    #   priority      = 103
-    #   policy_list   =[]
-    # }
-    # alertmanager = {
-    #   instance_type = "t3.small"
-    #   port          = 9093
-    #   priority      = 104
-    #   policy_list   =[]
-    # }
-    # artifactory = {
-    #   instance_type = "t3.medium"
-    #   port          = 8082
-    #   priority      = 105
-    #   policy_list   =[]
-    # }
+    sonarqube = {
+      instance_type = "t3.large"
+      port          = 9000
+      priority      = 100
+      policy_list   =[]
+    }
+    elasticsearch = {
+      instance_type = "m6in.large"
+      port          = 80
+      priority      = 101
+      policy_list   =[]
+    }
+    prometheus = {
+      instance_type = "t3.small"
+      port          = 9090
+      priority      = 102
+      policy_list   =[]
+    }
+    grafana = {
+      instance_type = "t3.small"
+      port          = 3000
+      priority      = 103
+      policy_list   =[]
+    }
+    alertmanager = {
+      instance_type = "t3.small"
+      port          = 9093
+      priority      = 104
+      policy_list   =[]
+    }
+    artifactory = {
+      instance_type = "t3.medium"
+      port          = 8082
+      priority      = 105
+      policy_list   =[]
+    }
     jenkins = {
       instance_type = "t3.medium"
       port          = 8080
