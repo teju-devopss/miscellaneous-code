dacc25cc37825f47d3d2fbb43a027f04ea675acf
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-08-05 15:51:48 +0530

    disabled prometheus stack from dervers we will have in EKS Directly

--- variables.tf
+++ variables.tf
@@ -12,30 +12,30 @@ variable "tools" {
       priority      = 101
       policy_list   =[]
     }
-    prometheus = {
-      instance_type = "t3.small"
-      port          = 9090
-      priority      = 102
-      policy_list   =[]
-    }
-    grafana = {
-      instance_type = "t3.small"
-      port          = 3000
-      priority      = 103
-      policy_list   =[]
-    }
+    # prometheus = {
+    #   instance_type = "t3.small"
+    #   port          = 9090
+    #   priority      = 102
+    #   policy_list   =[]
+    # }
+    # grafana = {
+    #   instance_type = "t3.small"
+    #   port          = 3000
+    #   priority      = 103
+    #   policy_list   =[]
+    # }
     alertmanager = {
       instance_type = "t3.small"
       port          = 9093
       priority      = 104
       policy_list   =[]
     }
-    artifactory = {
-      instance_type = "t3.medium"
-      port          = 8082
-      priority      = 105
-      policy_list   =[]
-    }
+    # artifactory = {
+    #   instance_type = "t3.medium"
+    #   port          = 8082
+    #   priority      = 105
+    #   policy_list   =[]
+    # }
     jenkins = {
       instance_type = "t3.medium"
       port          = 8080
